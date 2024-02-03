001b30d64cfe660986ce570b5fd835a24b3509e8
Muzaffar Nurillayev <nurillaewmuzaffar@gmail.com>
2024-02-03 14:22:06 +0500

    Add project to learn to manipulate with PDF

---
+++ IronPDF/Program.cs
@@ -0,0 +1,8 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        // PdfDocument document = PdfDocument.FromFile("/home/muzaffar/Programming/Books/dotnet-csharp.pdf");
+        
+    }
+}
